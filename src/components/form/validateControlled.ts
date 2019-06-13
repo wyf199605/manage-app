3d3abb76fd87cbbbf6d83456d1030a4a0a4580bc
翁一帆 <wengyf@fastlion.cn>
2019-06-13 16:51:03 +0800

    验证是否是受控组件

--- src/components/form/validateControlled.ts
+++ src/components/form/validateControlled.ts
@@ -2,6 +2,8 @@ import * as React from 'react';
 export const validateControlled = (target: React.Component, value = 'value', onChange = 'onChange') => {
     let props = target.props;
     if(value in props && !(onChange in props)){
-        throw new Error('受控 ' + target.constructor.name + ' 组件必须传入 ' + onChange + ' 参数！');
+        if(process.env.NODE_ENV !== 'production'){
+            console.error('受控 ' + target.constructor.name + ' 组件必须传入 ' + onChange + ' 参数！');
+        }
     }
 };
